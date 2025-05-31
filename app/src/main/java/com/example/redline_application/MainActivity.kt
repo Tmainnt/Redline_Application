85a3d98fb12afafe86a73519e2d9d85e9dc25337
Tmainnt <gamerthann@gmail.com>
2025-05-31 11:45:56 +0700

    craete new device, add button in file activity_main.xml, try to set activity in MainActivity.kt

--- app/src/main/java/com/example/redline_application/MainActivity.kt
+++ app/src/main/java/com/example/redline_application/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.redline_application
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import android.widget.Toast
+//import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity () {
     override fun onCreate(savedInstanceState : Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        cmdshow.setOnclickListener{ view->
+            tvOutput.text = "Hello Android Studio"
+            Toast.make.text(this, "Hello Android Studio", Toast.LENGHT_LONG).show()
+        }
     }
 }
